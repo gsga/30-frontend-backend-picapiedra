287a7b0a64cb21b38384bb34e69d3a2ce5702dfd
gsga <ggarciaa@gmail.com>
2022-06-13 17:14:25 -0300

    2022-06-13

--- backend/src/main/java/tup/backend/configuration/WebConfiguration.java
+++ backend/src/main/java/tup/backend/configuration/WebConfiguration.java
@@ -1,4 +1,4 @@
-package tup.backend.controller;
+package tup.backend.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- backend/src/main/java/tup/backend/controller/Controller.java
+++ backend/src/main/java/tup/backend/controller/Controller.java
@@ -1,10 +1,10 @@
-package com.example.demo.controller;
+package tup.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
+public class Controller {
 
     @GetMapping()
     public String home() {
